c9c43c6d35e23b35df7f1281f2683c68c52fe15b
wesley-lewis <lewis.wes.23@gmail.com>
2023-03-29 19:59:13 +0530

    Completed the account type challenge

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,41 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
+import * as Web3 from "@solana/web3.js"
+
+interface AccountInfo {
+  executable: boolean
+}
 
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [info, setInfo] = useState({})
 
-  const addressSubmittedHandler = (address: string) => {
-    setAddress(address)
-    setBalance(1000)
+  const addressSubmittedHandler = async (address: string) => {
+    try {
+      setAddress(address)
+      const key = new Web3.PublicKey(address)
+      const connection = new Web3.Connection(Web3.clusterApiUrl("devnet"))
+      connection.getBalance(key)
+        .then((balance) => {
+          setBalance(balance / Web3.LAMPORTS_PER_SOL)
+        })
+
+      // console.log(await connection.getAccountInfo(key))
+      connection.getAccountInfo(key)
+        .then((accInfo) => {
+          if (accInfo !== null) {
+            setInfo(accInfo)
+          }
+        })
+
+      setBalance(1000)
+    } catch (error) {
+      setAddress("")
+      setBalance(0)
+      alert(error);
+    }
   }
 
   return (
@@ -23,6 +50,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${info.executable}`}</p>
       </header>
     </div>
   )
